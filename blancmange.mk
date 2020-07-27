d86b22905a9c6b72939db3e14182712781cd2d8b
Robin Jones <smithtrombone@gmail.com>
2020-07-27 04:08:12 -0400

    ...

--- blancmange.mk
+++ blancmange.mk
@@ -1,10 +1,8 @@
 Blancmange 64
 ===
 
-Blancmange 64 is an esoteric interpreted programming language. 
-Blancmange is inspired by Befunge-93.
+Blancmange 64 is an esoteric interpreted programming language inspired by Befunge-93. 
 Blancmange's main difference from Befunge is that it is intended to be an entirely serious language for use in the real world.
-Blancmange is stack based and reflective (as in assembly).
 
 ---
 
@@ -14,7 +12,6 @@ Blancmange is stack based and reflective (as in assembly).
 The Program Counter marks the current instruction.
 Unlike conventional program counters, the B64 PC is able to move forward, backward, upward, and downward.
 The PC moves through a torus of 2^16-1 x 2^16-1 bytes.
-The size of the torus can be adjusted during the start of the environment.
 
 # Concurrency
 
@@ -24,11 +21,11 @@ The initial Program Counter always has an ID of 0.
 
 # Torus Coordinates
 
-The torus coordinates can begin at the upper left corner at 0x0.
+The torus coordinates begin at the upper left corner at 0x0.
 Coordinates are compressed into the two least significant 16 bit values in an integer type.
 For example, an integer holding the value 65,356 represents the coordinates 1,0 in the torus.
 
-# Program Counter Instructions
+## Program Counter Instructions
 
 *  ... Whitespace represents no operation. The current direction is continued.
 * ^... Change direction upwards.
@@ -40,9 +37,10 @@ For example, an integer holding the value 65,356 represents the coordinates 1,0 
 
 * !... spawn a new PC.
 * ~... Pop x and pause the PC with the matching character.
-* x... Pop x and end PC with the matching character.
-* f... Pop x and push x with PC identifier
-* F... Pop x and push x with a byte representing the total number of Program Counters running.
+* ,... Pop x and resume the PC with the matching character.
+* \`... Pop x and end the PC with the matching character.
+* t... Pop x and push x with PC identifier
+* T... Pop x and push x with a byte representing the total number of Program Counters running.
 
 ---
 
@@ -57,13 +55,13 @@ Operations are performed using two registers as X and Y operands.
 
 Registers A-F are used for unsigned integer operations
 
-# Register Operations
+## Register Operations
 
 * 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F... Sets the current register.
 * i... Load the integer following this instruction into the current register.
 * c... Load the character following this instruction into the current register.
-* x... Load a zero torus X coordinate into the current register.
-* y... Load a zero torus Y coordinate into the current register.
+* X... Load a zero torus X coordinate into the current register.
+* Y... Load a zero torus Y coordinate into the current register.
 * Q... Loads the current X,Y position if the PC onto the register.
 * P... Pushes the current register onto the stack.
 
@@ -75,11 +73,11 @@ The Stack is a LIFO structure used to control access to registers for operations
 In order for a register to be used in an operation, its number mustbe pushed onto the stack.
 Instructions that operate on register data will know which registers to use by popping register numbers off the stack and accessing the corresponding registers.
 
-# Stack Instructions
+## Stack Instructions
 
 * p... Pop a register from the stack and do nothing.
 * :... Duplicate the top of the stack.
-* \... Switch the top two Datums on the stack.
+* \\... Switch the top two Datums on the stack.
 
 ---
 
@@ -87,23 +85,24 @@ Instructions that operate on register data will know which registers to use by p
 
 Blancmange supports three data types, signed and unsigned 64 bit words and the unsigned byte.
 These types can be used interchangeably.
+Signed and Unsigned Words are converted by copying between registers.
 Copying a word to a character will only preserve the least significant byte.
-Unsigned Words are useful for handling memory addresses outside of the torus.
+únsigned Words are used for coordinates and memory addresses outside of the torus and anywhere else where two's compliment won't make sense.
 The torus only supports information in unsigned bytes, so word types are stored across a series of eight bytes in little endian format.
 
-# Data Instructions
+## Data Instructions
 
 All instructions below will pop register y, then pop register x, then push register x with the result.
 
 * +... x + y
 * -... x - y
-* *... x * y
-* /... x / y
-* %... x % y
+* \*... x \* y
+* \/... x \/ y
+* \%... x % y
 
 * &... Binary AND of x and y
-* r... Binary OR of x and y
-* n... Binary NOT of the top
+* O... Binary OR of x and y
+* N... Binary NOT of the top
 * !... Logical NOT of the top. If top is zero, a max integer value is pushed. If top is non-zero, a zeroed integer is pushed.
 
 * C... copy y to x.
@@ -123,16 +122,16 @@ Comparison instructions pop register y, then pop register x, then set the Branch
 * ?... If true execute the next instruction. If false, skip the next instruction.
 * |... If true, emulate ^. If false, emulate v.
 * _... If true, emulate <. If false, emulate >.
-* #... Unconditionally skips the next instruction.
+* \#... Unconditionally skips the next instruction.
 
 ---
 
 # Input and Output
 
-Blancmange programs a guaranteed to always have a default input and output stream.
-These streams are formatted so that characters and integers can both be read and written to them.
+Blancmange programs are guaranteed to always have a default input and output stream.
+These streams are formatted so that characters and integers can both be read and written to them without requiring conversion.
 
-# Input and Output Instructions
+## Input and Output Instructions
 
 Both Read and Write pop a register from the top of the stack.
 
@@ -147,7 +146,7 @@ There is a set of special read and write instructions that allow Blancmange to r
 * R... Pops a register from the stack holding coordinates. Places the integer found at those coordinates in the popped register and pushes it.
 * W... Pops registers y and x from the stack and writes the integer in x at the coordinates y.
 
-Integers are written in little endian format in the current direction of the PC.
+Integers are read and written in little endian format in the current direction of the PC.
 
 ---
 
@@ -163,108 +162,58 @@ Blancmange offers multiple pre-defined devices to allow access to various resour
 
 ---
 
-# The Page Device
+## The Page Device
 
 If a Blancmange program requires memory beyond what is available in the torus, the Page object can be used.
 The Page object represents a memory segment of 1KB.
 It has a 1x9 byte mapping with a byte the left and an integer on the left.
-[read][write]
+ 0     1     9
+0[read][write]
 The first byte can be read from.
 It represents a byte from the array.
 The integer represents the address of the byte in the Page.
 The address can be updated by writing a new integer to it.
 
-# Page Device Instructions
+### Page Device Instructions
 
-* Z... Pops a register with coordinates to construct the device.
+* Z... Pops x and y. Creates page with x bytes at y coordinates.
 * z... Pops a register with the coordinates of a Page to destroy.
 
-
 ---
 
-# Program Counter Instructions
+## The File Device
 
-- PC UP
-- PC DOWN
-- PC LEFT
-- PC RIGHT
+The File device lets Blancmange read and write to the filesystem.
+The File device has a 2x? byte mapping.
 
-- BRANCH UP
-- BRANCH DOWN
-- BRANCH LEFT
-- BRANCH RIGHT
+ 0          1        2      3            11
+0[rwa flags][message][Error][buffer size][buffer location]
+1[file pathname
 
-- JUMP
+rwa flags is a single byte state flag that can be written to.
 
----
+### Flags
 
-#Stack Manipulation
+Bit Representation
+1. read only 
+2. write only 
+4.. read and write
+8. write append
+16. overwrite
 
-- LOAD
-- 
-- SAVE
-- 
+### Messages
 
----
+* r... Read buffer size bytes from file and place in buffer
+* w... Write buffer size bytes to file from buffer
+* o... Open file
+* c... Close file
 
-#Stack Math Instructions
+### Error Flags
 
-- ADD 
-- SUB
-- MUL
-- DIV
-- MOD
-
----
-
-- AND
-- OR
-- NOT
-
-- EQUAL
-- NOT EQUAL
-- LESS THAN
-- GREATER THAN
+* x... File couldn't be opened
+* w... File couldn't be written
+* r... File couldn't be read
+* m... not enough storage
 
 ---
-
-- DUPLICATE STACK
-- SWAP STACK
-- POP STACK
-- PUSH STACK
-
----
-
-- READ
-- WRITE
-
----
-
-- CREATE PAGE
-- FREE PAGE
-
----
-
-- CREATE NET STREAM
-- FREE NET STREAM
-
----
-
-- CREATE FILE STREAM
-- FREE FILE STREAM
-
----
-
-- SPAWN PC
-- PAUSE PC
-- STOP PC
-
----
-
-- GET
-- SET
-
----
-
-- MOVE FROM TO
 
